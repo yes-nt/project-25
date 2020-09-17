b3fb6c67d20b9690aa9bde9a42504b5603873e83
yes-nt <70011254+yes-nt@users.noreply.github.com>
2020-09-18 00:20:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,13 +3,11 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var paperImg,binImg
+
 
 function preload()
 {
-	paperImg = loadImage("paper.png");
-	binImg = loadImage("dustbingreen.png");
-}
+
 
 function setup() {
 	createCanvas(800, 700);
@@ -20,11 +18,11 @@ function setup() {
 
 	//creates the paper
 	paper = new Paper (200,120);
-	paper.addImage(paperImg);
+
 
 	//creates the dustbin
 	dustbin = new Dustbin (400,591,150,100);
-	dustbin.addImage(binImg);
+	
 	
 	//creates the ground
 	ground = new Ground (400,650,800,20);
@@ -44,10 +42,6 @@ function draw() {
   paper.display();
   dustbin.display();
   ground.display();
-
-	if(paper.y-dustbin.y<paper.height/2+dustbin.height/2){
-		paper.velocityY = 10;
-	}
 
   
   
